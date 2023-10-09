4693a766fa1a7a2ab0af538752254f778bc81069
eaTaki <ea.takiguchi@gmail.com>
2023-10-09 23:52:27 +0300

    initial commit

---
+++ Coursework1/generate_pdf_from_ipynb.bat
@@ -0,0 +1,10 @@
+@echo off
+
+if "%~1"=="" (
+    echo Please provide an exercise number as an argument.
+    exit /b 1
+)
+
+set PATH=%PATH%;C:\Users\etaki\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts
+
+jupyter nbconvert --to pdf "cw1ex%1\Coursework 1 - Exercise %1.ipynb"
