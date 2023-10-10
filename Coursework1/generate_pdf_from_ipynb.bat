0ce12b22a6b3d37b09fdec69afb93e81bf527d05
eaTaki <ea.takiguchi@gmail.com>
2023-10-10 12:51:29 +0300

    done exercises 5,6

--- Coursework1/generate_pdf_from_ipynb.bat
+++ Coursework1/generate_pdf_from_ipynb.bat
@@ -7,4 +7,4 @@ if "%~1"=="" (
 
 set PATH=%PATH%;C:\Users\etaki\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts
 
-jupyter nbconvert --to pdf "cw1ex%1\Coursework 1 - Exercise %1.ipynb"
+jupyter nbconvert --to pdf "Coursework1\cw1ex%1\Coursework 1 - Exercise %1.ipynb"
